277a5cfda188b3f8787243a04a44880f1b4648db
Oybek159 <oybekxalimov159@gmail.com>
2025-07-22 12:18:01 +0500

    first

--- study/studyapp/views.py
+++ study/studyapp/views.py
@@ -105,32 +105,42 @@ def userProfile(request, pk):
 
 @login_required(login_url='login')
 def createroom(request):
+    topics = Topic.objects.all()
     form = RoomForm()
     if request.method == 'POST':
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-            return redirect('home')
-    context = {'form': form}
+        topic_name = request.POST.get('topic')
+        topic, created_at = Topic.objects.get_or_create(name=topic_name)
+
+        Room.objects.create(
+            host = request.user,
+            topic=topic,
+            name=request.POST.get('name'),
+            description= request.POST.get('description')
+        )
+        
+        return redirect('home')
+    context = {'form': form, 'topics': topics}
     return render(request, 'studyapp/create_room.html', context)
 
 @login_required(login_url='login')
 def updateroom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+    topics = Topic.objects.all()
 
     if request.user != room.host:
         return HttpResponse("You're not allowed here!")
 
     if request.method == 'POST':
-        form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    context = {'form': form}
-    return render(request, 'studyapp/room_form.html', context)
+        topic_name = request.POST.get('topic')
+        topic, created_at = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()
+        return redirect('home')
+    context = {'form': form, 'topics': topics, 'room': room}
+    return render(request, 'studyapp/create_room.html', context)
 
 @login_required(login_url='login')
 def deleteroom(request, pk):
